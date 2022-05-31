8e4d8a7047335255c452020a6e6efd3956b76e76
Lucas A <lucasalencargomes111@gmail.com>
2022-05-31 15:53:41 -0300

    upload

---
+++ main.bat
@@ -0,0 +1,41 @@
+title GitHub upload
+
+echo off && cls
+
+echo ""
+echo "upar os teus projeto para o github bem rapidinho"
+echo ""
+set /p project_DIR="Insira o caminho do seu projeto >> ";
+
+set /p nome="Insira o seu nome >> ";
+
+set /p email="Insira o seu email >> ";
+
+set /p mensagem="Insira a mensagem do commit >> ";
+
+set /p projeto_link="Insira o link do seu projeto (ex: https://github.com/user/repo.git) >> ";
+
+cd "%project_DIR%"
+
+git init
+echo ""
+
+git config --global user.name "%nome%"
+echo ""
+
+git config --global user.email "%email%"
+echo ""
+
+git add .
+echo ""
+
+git commit -m "%mensagem%"
+echo ""
+
+git remote add origin %projeto_link%
+echo ""
+
+git push origin master
+echo ""
+
+pause
